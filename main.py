ae14e25cad18913ce93fb1f70ea1bafc2f1e2bed
Adam Skorek <askorek@gmail.com>
2023-10-23 11:26:24 +0200

    starting from main podcasts page

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import requests as r
-import re
+from bs4 import BeautifulSoup
 
-main_page = r.get("https://pulsembed.eu/p2em/q5JTqWkbV/")
+main_page = r.get("https://sites.libsyn.com/402971")
 main_html = str(main_page.content)
-patt = r'src="\/\/(.+\/)"'
-second_page_url = re.search(patt, main_html)[1]
-second_page = r.get("https://" + second_page_url)
+
+soup = BeautifulSoup(main_html, 'html.parser')
+podcasts_links = [el['href'] for el in soup.find_all('a', {"class": "read_more"})]
+
+for href in podcasts_links:
+        single_podcast_page = r.get(href)
+        single_podcast_html = str(single_podcast_page.content.decode("utf-8"))
+        soup_podcast = BeautifulSoup(single_podcast_html, 'html.parser')
+        podcast_title = soup_podcast.title.text
+
+        with open('downloaded.txt') as already_downloaded:
+            all_downloaded = already_downloaded.readlines()
+            if podcast_title in all_downloaded:
+                continue
 
 
 
 
-print("dupa")
+
+        download_link = soup_podcast.find('meta', {"name": "twitter:player:stream"})['content']
+        file = r.get(download_link)
+
+        with open(podcast_title + '.mp3', mode='wb') as new_file:
+            new_file.write(file.content)
+
+
+        print("dupa")
