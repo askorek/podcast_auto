c08a678d804a9f6bfa963f273ad925d11173963d
Adam Skorek <askorek@gmail.com>
2023-11-01 10:51:19 +0100

    changed script flow. started adding rss

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import requests as r
 from bs4 import BeautifulSoup
 import re
 import rfeed
+from pprint import pp
 
-def get_podcast_links():
-    main_page = r.get("https://sites.libsyn.com/402971")
-    main_html = str(main_page.content)
+MAIN_PAGE = "https://sites.libsyn.com/402971"
+DOWNLOADED_FILE = 'downloaded.txt'
+RSS_FILENAME = 'podcast.rss'
+RSS_TITLE = 'Raport Międzynarodowy'
+MY_PAGE_ADRESS =
 
-    soup = BeautifulSoup(main_html, 'html.parser')
-    podcasts_links = [el['href'] for el in soup.find_all('a', {"class": "read_more"})]
+def check_if_there_are_updates(url, downloaded_txt_file):
+    podcasts_links = get_podcasts_links(url)
 
-    return podcasts_links
-
-
-def check_if_already_downloaded_and_download_if_not(podcast_pages_list):
-    podcast_pages_list = list(dict.fromkeys(podcast_pages_list)) # remove duplicates, preserve order
-
-    with open('downloaded.txt', 'w+') as already_downloaded:
+    with open(downloaded_txt_file, 'w+') as already_downloaded:
         all_downloaded = already_downloaded.readlines()
 
-    for podcast_link in podcast_pages_list:
+    for podcast_link in podcasts_links:
         if podcast_link in all_downloaded:
             continue
         else:
-            download_single_podcast(podcast_link)
-            with open('downloaded.txt', 'a') as already_downloaded:
-                already_downloaded.writelines(podcast_link + '\n')
+            return True
+    return False
+
+# !!!!!
+def check_if_already_downloaded_and_download_if_not(podcast_link, donwloaded_file):
+    with open(donwloaded_file, 'w+') as already_downloaded:
+        all_downloaded = already_downloaded.readlines()
+
+    if podcast_link in all_downloaded:
+        return
+    else:
+        download_single_podcast(podcast_link)
+        with open(donwloaded_file, 'a') as already_downloaded:
+            already_downloaded.writelines(podcast_link + '\n')
 
 
 def download_single_podcast(podcast_page_url):
@@ -58,6 +66,7 @@ def get_podcast_metadata(podcast_link):
     date_regex = r'\/([0-9]+)_RAPORT'
     date = re.search(date_regex, mp3_link)[1]
 
+    metadata["podcast_link"] = podcast_link
     metadata["mp3_filename"] = mp3_filename
     metadata["mp3_link"] = mp3_link
     metadata["podcast_title"] = podcast_title
@@ -66,14 +75,68 @@ def get_podcast_metadata(podcast_link):
 
     return metadata
 
-aa = get_podcast_metadata('https://sites.libsyn.com/402971/izrael-traci-status-ofiary-europa-na-pewno-nie-jest-bezpieczna-po-tym-co-dzieje-si-na-bliskim-wschodzie')
-print(aa)
+def get_all_podcasts_metadata(url):
+    podcasts_links = get_podcasts_links(url)
+
+    all_metadata = list()
+    for link in podcasts_links:
+        metadata = get_podcast_metadata(link)
+        all_metadata.append(metadata)
+
+    return all_metadata
+
+
+def get_podcasts_links(url):
+    main_page = r.get(url)
+    main_html = str(main_page.content.decode("utf-8"))
+    soup = BeautifulSoup(main_html, 'html.parser')
+    podcasts_links = [el['href'] for el in soup.find_all('a', {"class": "read_more"})]
+    podcasts_links = list(dict.fromkeys(podcasts_links))  # remove duplicates, preserve order
+    return podcasts_links
+
+
+def create_rss_file(all_metadata, rss_filename, title, my_page_adress):
+
+    _items = list()
+    for metadata in all_metadata:
+        item = rfeed.Item(
+            title=metadata['podcast_title'],
+            link=metadata['podcast_link'],
+            description=metadata['podcast_description'],
+            guid=metadata['podcast_link'],
+            enclosure = Enclosure(url="http://www.example.com/articles/1.mp3", length=0, type='audio/mpeg')
+        )
+
+    feed = rfeed.Feed(title=title)
+
+
+def main():
+    if not check_if_there_are_updates(MAIN_PAGE, DOWNLOADED_FILE, MY_PAGE_ADRESS):
+        return 0
+
+    all_metadata = get_all_podcasts_metadata(MAIN_PAGE)
+
+    #download missing files
+    for metadata in all_metadata:
+        link = metadata['podcast_link']
+        check_if_already_downloaded_and_download_if_not(link, DOWNLOADED_FILE)
+
+    create_rss_file(all_metadata, RSS_FILENAME, RSS_TITLE)
+
+    pp(all_metadata)
+
+
+
+
+main()
+
+#aa = get_podcast_metadata('https://sites.libsyn.com/402971/izrael-traci-status-ofiary-europa-na-pewno-nie-jest-bezpieczna-po-tym-co-dzieje-si-na-bliskim-wschodzie')
+#print(aa)
 
 # podcasts_links = get_podcast_links()
 # check_if_already_downloaded_and_download_if_not(podcasts_links)
 
 
 # TODO:
-# get podcast metadata (title, description)
 # create rss file
 # update rss file
